66d2504377d6cf28ed816d6b3ed02121aa5662ae
Mukund Solanki <mukundsolanki665@gmail.com>
2024-10-11 20:17:01 +0530

    Notification service and overlay

--- app/src/main/java/com/example/sleepplay/MainActivity.kt
+++ app/src/main/java/com/example/sleepplay/MainActivity.kt
@@ -2,13 +2,22 @@ package com.example.sleepplay
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.ComponentName
 import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
+import android.text.TextUtils
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
@@ -18,7 +27,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
 
@@ -26,19 +34,33 @@ class MainActivity : ComponentActivity() {
     private val CHANNEL_ID = "SleepPlayChannel"
     private val NOTIFICATION_ID = 1
 
+    private val stopAppReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            if (intent?.action == "STOP_APP") {
+                finish()
+            }
+        }
+    }
+
     private val requestPermissionLauncher = registerForActivityResult(
         ActivityResultContracts.RequestPermission()
     ) { isGranted: Boolean ->
         if (isGranted) {
-            showNotification(this)
+            showNotification()
         } else {
-            // Handle the case where permission is not granted
+
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         createNotificationChannel()
+        checkOverlayPermission()
+        checkAccessibilityPermission()
+
+        registerReceiver(stopAppReceiver, IntentFilter("STOP_APP"), RECEIVER_NOT_EXPORTED)
+
         setContent {
             MaterialTheme {
                 Surface(
@@ -51,12 +73,30 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(stopAppReceiver)
+
+        val intent = Intent(this, BlackScreenService::class.java)
+        stopService(intent)
+    }
+
     @Composable
     fun SleepPlayApp() {
         Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
             Button(onClick = { checkNotificationPermission() }) {
                 Text("Start")
             }
+        }
+    }
+
+    private fun checkOverlayPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
+            val intent = Intent(
+                Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                Uri.parse("package:$packageName")
+            )
+            startActivityForResult(intent, OVERLAY_PERMISSION_REQ_CODE)
         }
     }
 
@@ -67,16 +107,46 @@ class MainActivity : ComponentActivity() {
                     this,
                     android.Manifest.permission.POST_NOTIFICATIONS
                 ) == PackageManager.PERMISSION_GRANTED -> {
-                    showNotification(this)
+                    showNotification()
                 }
+
                 else -> {
                     requestPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
                 }
             }
         } else {
-            showNotification(this)
+            showNotification()
         }
     }
+
+    private fun checkAccessibilityPermission() {
+        if (!isAccessibilityServiceEnabled(this, KeyEventAccessibilityService::class.java)) {
+            val intent = Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS)
+            startActivity(intent)
+        }
+    }
+
+    private fun isAccessibilityServiceEnabled(context: Context, service: Class<*>): Boolean {
+        val enabledServicesSetting = Settings.Secure.getString(
+            context.contentResolver,
+            Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES
+        ) ?: return false
+
+        val colonSplitter = TextUtils.SimpleStringSplitter(':')
+        colonSplitter.setString(enabledServicesSetting)
+
+        while (colonSplitter.hasNext()) {
+            val componentNameString = colonSplitter.next()
+            val enabledService = ComponentName.unflattenFromString(componentNameString)
+            if (enabledService != null && enabledService.packageName == context.packageName &&
+                enabledService.className == service.name
+            ) {
+                return true
+            }
+        }
+        return false
+    }
+
 
     private fun createNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
@@ -92,17 +162,48 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    private fun showNotification(context: Context) {
-        val builder = NotificationCompat.Builder(context, CHANNEL_ID)
+    private fun showNotification() {
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent: PendingIntent =
+            PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+
+        val sleepIntent = Intent(this, BlackScreenService::class.java).apply {
+            action = "SHOW_BLACK_SCREEN"
+        }
+        val sleepPendingIntent: PendingIntent = PendingIntent.getService(
+            this,
+            1,
+            sleepIntent,
+            PendingIntent.FLAG_IMMUTABLE
+        )
+
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
             .setSmallIcon(android.R.drawable.ic_dialog_info)
             .setContentTitle("SleepPlay")
-            .setContentText("Hello World")
+            .setContentText("Click Sleep to show black screen overlay")
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .addAction(android.R.drawable.ic_menu_close_clear_cancel, "Sleep", sleepPendingIntent)
             .setAutoCancel(true)
 
         val notificationManager: NotificationManager =
-            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
         notificationManager.notify(NOTIFICATION_ID, builder.build())
+    }
+
+    companion object {
+        private const val OVERLAY_PERMISSION_REQ_CODE = 1
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == OVERLAY_PERMISSION_REQ_CODE) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
+
+            }
+        }
     }
 }
